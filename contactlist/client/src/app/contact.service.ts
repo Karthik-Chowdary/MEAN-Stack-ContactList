fbf8159783c1d7340176ebcadaf5ba0382226202
Karthik Chowdary <svskarthikchowdary@gmail.com>
2018-03-04 11:56:52 -1000

    Add files via upload

--- contactlist/client/src/app/contact.service.ts
+++ contactlist/client/src/app/contact.service.ts
@@ -8,10 +8,21 @@ export class ContactService {
 
   constructor(private http: Http) { }
 
-//Retriving contacts
-getcontacts(){
-return this.http.get('http://localhost:3000/api/contacts')
-.map(res => res.json());
-}
+  getContacts(){
+    return this.http.get('http://localhost:3000/api/contacts')
+    .map(res=> res.json());
+  }
+
+  addContact(newContact){
+   var headers = new Headers();
+   headers.append('Content-Type','application/json');
+   return this.http.post('http://localhost:3000/api/contacts', newContact, {headers: headers})
+   .map(res=> res.json());
+  }
+
+  deleteContact(id){
+    return this.http.delete('http://localhost:3000/api/contacts/'+id)
+    .map(res=>res.json());
+  }
 
 }
